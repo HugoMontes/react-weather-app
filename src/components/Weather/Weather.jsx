88a832f622991ad2665304311e5cba7fcf78c57b
hmontes <hmontes@cofar.com.bo>
2021-01-18 16:48:00 -0400

    Instalando iconos

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,11 +3,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
+// Importar la libreria de iconos de clima
+import { WiCloud } from 'react-icons/wi'
+// Importar un proveedor de contexto para poner
+// un tamaño al icono
+import { IconContext } from 'react-icons'
 
 const Weather = ({ temperature }) => {
     return (
         <div>
-            <Typography variant="h2">{temperature}</Typography>
+            <IconContext.Provider value={{ size: '5em'}}>
+                <WiCloud></WiCloud>
+            </IconContext.Provider>
+            <Typography display="inline" variant="h3">{temperature}</Typography>
         </div>
     )
 }
