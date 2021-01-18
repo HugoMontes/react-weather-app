8a1179e6e4b2986d5ff6adf7eeb85cbd5ba8fc3f
hmontes <hmontes@cofar.com.bo>
2021-01-18 12:11:21 -0400

    Adicionar una historia para CityInfo

---
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -0,0 +1,12 @@
+// importar el componente React "imr" + TAB
+import React from 'react'
+// Importar el componente CityInfo
+import CityInfo from './CityInfo'
+// Exportar el componente enviando un titulo y el coponente
+export default {
+    title: "CityInfo",
+    component: CityInfo
+}
+// Agregar los subitems, diferentes formas
+// en las que el componente se puede visualizar
+export const CityExample = () => <CityInfo city={"La Paz"} country={"Bolivia"}></CityInfo>
