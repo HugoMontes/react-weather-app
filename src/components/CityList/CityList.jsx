c2f162489223d0eaad40777fb6d44ccb965c57a6
hmontes <hmontes@cofar.com.bo>
2021-01-20 11:52:26 -0400

    Corregir grilla responsive

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -19,10 +19,10 @@ const renderCityAndCountry = cityAndCountry => {
             <Grid container
                 justify="center"
                 alignItems="center">
-                    <Grid item sm={8}>
+                    <Grid item md={8} xs={12}>
                         <CityInfo city={city} country={country} />
                     </Grid>
-                    <Grid item sm={4}>
+                    <Grid item md={4} xs={12}>
                         <Weather temperature={10} state="sunny"/>
                     </Grid>
             </Grid>
